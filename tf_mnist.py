1fc1134c66e1581ebbefc2e6a26644324c0a20b4
Viniciuscoding <vin.idem@gmail.com>
2021-05-29 10:23:53 -0400

    Create tf_mnist.py

---
+++ tf_mnist.py
@@ -0,0 +1,84 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+def get_weights(features, labels):
+    # Return Tensorflow weights
+    return tf.Variable(tf.truncated_normal((features, labels)))
+
+def get_biases(labels):
+    # Return Tensorflow biases
+    return tf.Variable(tf.zeros(labels))
+
+def linear(tf_input, tf_weights, tf_biases):
+    # Return a linear function result
+    # Linear function = xW + b
+    # tf.matmul it a matrix multiplication in Tensorflow
+    return tf.add(tf.matmul(tf_input, tf_weights), tf_biases)
+
+def mnist_features_labels(labels, n_images = 5000):
+
+    mnist_features, mnist_labels = [], []
+
+    mnist = input_data.read_data_sets('ADD your MNIST image data dir here', one_hot = True)
+
+    for mnist_feature, mnist_label in zip(*mnist.train.next_batch(n_images)):
+        # Add features and labels if it's for the first <n>th labels
+        if mnist_label[:labels].any():
+            mnist_features.append(mnist_feature)
+            mnist_labels.append(mnist_label[:labels])
+    
+    # Return a tf tuple of feature and label list
+    return mnist_features, mnist_labels
+
+# FINISH WORKING ON THE MAIN FUNCTION
+
+# Number of features (28*28 image is 784 features)
+n_features = 784
+# Number of labels
+n_labels = 3
+
+# Features and Labels
+features = tf.placeholder(tf.float32)
+labels = tf.placeholder(tf.float32)
+
+# Weights and Biases
+w = get_weights(n_features, n_labels)
+b = get_biases(n_labels)
+
+# Linear Function xW + b
+logits = linear(features, w, b)
+
+# Training data
+train_features, train_labels = mnist_features_labels(n_labels)
+
+with tf.Session() as session:
+    # TODO: Initialize session variables
+    session.run(tf.global_variables_initializer())
+    # Softmax
+    prediction = tf.nn.softmax(logits)
+
+    # Cross entropy
+    # This quantifies how far off the predictions were.
+    # You'll learn more about this in future lessons.
+    cross_entropy = -tf.reduce_sum(labels * tf.log(prediction), reduction_indices=1)
+
+    # Training loss
+    # You'll learn more about this in future lessons.
+    loss = tf.reduce_mean(cross_entropy)
+
+    # Rate at which the weights are changed
+    # You'll learn more about this in future lessons.
+    learning_rate = 0.08
+
+    # Gradient Descent
+    # This is the method used to train the model
+    # You'll learn more about this in future lessons.
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+
+    # Run optimizer and get loss
+    _, l = session.run(
+        [optimizer, loss],
+        feed_dict={features: train_features, labels: train_labels})
+
+# Print loss
+print('Loss: {}'.format(l))
